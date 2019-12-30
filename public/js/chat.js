c6a6b52997b8b733df504d47d91d8496244fad47
anjali1903 <anjali.jha1903@gmail.com>
2019-12-31 00:40:49 +0530

    Add autoscroll

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,31 +1,74 @@
 const socket = io();
-
+//Elements
 const $messageForm = document.querySelector("#message-form");
 const $messageFormInput = $messageForm.querySelector("input");
 const $messageFormButton = $messageForm.querySelector("button");
 const $sendLocationButton = document.querySelector("#location");
 const $messages = document.querySelector("#messages");
 
+//Templates
 const messageTemplate = document.querySelector("#message-template").innerHTML;
 const locationMessageTemplate = document.querySelector(
   "#location-message-template"
 ).innerHTML;
+const sideBarTemplate = document.querySelector("#sidebar-template").innerHTML;
+
+//Options
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true
+});
+
+const autoscroll = () => {
+  //New message element
+  const $newMessage = $messages.lastElementChild;
+
+  //Height of new message
+  const newMessageStyles = getComputedStyle($newMessage);
+  const newMessageMargin = parseInt(newMessageStyles.marginBottom);
+  const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
+
+  //Visible Height
+  const visibleHeight = $messages.offsetHeight;
+
+  //Height of messages container
+  const containerHeight = $messages.scrollHeight;
+
+  //How far scrolled
+  const scrollOffset = $messages.scrollTop + visibleHeight;
+
+  if (containerHeight - newMessageHeight <= scrollOffset) {
+    $messages.scrollTop = $messages.scrollHeight;
+  }
+};
+
 socket.on("message", message => {
   console.log(message);
   const html = Mustache.render(messageTemplate, {
+    username: message.username,
     createdAt: moment(message.createdAt).format("DD MMM h:mm a"),
     message: message.text
   });
   $messages.insertAdjacentHTML("beforeend", html);
+  autoscroll();
 });
 
 socket.on("locationMessage", message => {
   console.log(message);
   const html = Mustache.render(locationMessageTemplate, {
+    username: message.username,
     url: message.url,
-    createdAt: moment(message.createdAt).format("DD MM h:mm a")
+    createdAt: moment(message.createdAt).format("DD MMM h:mm a")
   });
   $messages.insertAdjacentHTML("beforeend", html);
+  autoscroll();
+});
+
+socket.on("roomData", ({ room, users }) => {
+  const html = Mustache.render(sideBarTemplate, {
+    room,
+    users
+  });
+  document.querySelector("#sidebar").innerHTML = html;
 });
 
 $messageForm.addEventListener("submit", e => {
@@ -46,17 +89,30 @@ $messageForm.addEventListener("submit", e => {
 });
 
 $sendLocationButton.addEventListener("click", () => {
-  $sendLocationButton.setAttribute("disable", "disabled");
   if (!navigator.geolocation) {
-    return alert("Geolocation not supported by your browser");
+    return alert("Geolocation is not supported by your browser.");
   }
-  navigator.geolocation.getCurrentPosition(position => {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
 
-    socket.emit("sendLocation", { latitude, longitude }, () => {
-      console.log("Location shared!");
-      $sendLocationButton.removeAttribute("disabled");
-    });
+  $sendLocationButton.setAttribute("disabled", "disabled");
+
+  navigator.geolocation.getCurrentPosition(position => {
+    socket.emit(
+      "sendLocation",
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      },
+      () => {
+        $sendLocationButton.removeAttribute("disabled");
+        console.log("Location shared!");
+      }
+    );
   });
+});
+
+socket.emit("join", { username, room }, error => {
+  if (error) {
+    alert(error);
+    location.href = "/";
+  }
 });
--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ const {
   generateMessage,
   generateLocationMessage
 } = require("./utils/messages");
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom
+} = require("./utils/users");
 
 const app = express();
 const server = http.createServer(app);
@@ -20,22 +26,43 @@ app.use(express.static(publicDirectoryPath));
 io.on("connection", socket => {
   console.log("New web socket connection");
 
-  socket.emit("message", generateMessage("You are welcome!"));
-  socket.broadcast.emit("message", generateMessage("A new user has joined!"));
+  socket.on("join", (options, callback) => {
+    const { error, user } = addUser({ id: socket.id, ...options });
+    if (error) {
+      return callback(error);
+    }
+    socket.join(user.room);
+
+    socket.emit("message", generateMessage("Admin", "You are welcome!"));
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        generateMessage(`${user.username} has joined the chat!`)
+      );
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room)
+    });
+    callback();
+  });
 
   socket.on("sendMessage", (message, callback) => {
     const filter = new Filter();
     if (filter.isProfane(message)) {
       return callback("Profanity is not allowed!");
     }
-    io.emit("message", generateMessage(message));
+    const user = getUser(socket.id);
+    io.to(user.room).emit("message", generateMessage(user.username, message));
     callback();
   });
 
   socket.on("sendLocation", (coords, callback) => {
-    io.emit(
-      "message",
+    const user = getUser(socket.id);
+    io.to(user.room).emit(
+      "locationMessage",
       generateLocationMessage(
+        user.username,
         `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
       )
     );
@@ -43,7 +70,17 @@ io.on("connection", socket => {
   });
 
   socket.on("disconnect", () => {
-    io.emit("message", generateMessage("A user has left!"));
+    const user = removeUser(socket.id);
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        generateMessage("Admin", `${user.username} has left the chat!`)
+      );
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room)
+      });
+    }
   });
 });
 
